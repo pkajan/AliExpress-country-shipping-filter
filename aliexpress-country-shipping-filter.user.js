d022e5a0a4f23d807f7c12fa02ec69245be2b4ba
Peter Kajan <sknitro@gmail.com>
2022-08-09 13:25:37 +0200

    combined delivery
    add combined delivery support

--- aliexpress-country-shipping-filter.user.js
+++ aliexpress-country-shipping-filter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AliExpress country shipping filter
 // @namespace    pk-scripts
-// @version      0.4
+// @version      0.5
 // @description  try to take over the AliExpress!
 // @author       pKajan
 // @match        *://*.aliexpress.com/*SearchText=*
@@ -14,13 +14,13 @@
 // @updateURL    https://github.com/pkajan/AliExpress-country-shipping-filter/raw/main/aliexpress-country-shipping-filter.user.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     function getCookie(cname) {
         let name = cname + "=";
         let decodedCookie = decodeURIComponent(document.cookie);
         let ca = decodedCookie.split(';');
-        for(let i = 0; i <ca.length; i++) {
+        for (let i = 0; i < ca.length; i++) {
             let c = ca[i];
             while (c.charAt(0) == ' ') {
                 c = c.substring(1);
@@ -40,7 +40,7 @@
     var aStyle = "color: #ff5733;";
     var bStyle = "color: #dbeb34; font-weight: bold;";
 
-    Object.keys(countriesAssociative).forEach(function(key) {
+    Object.keys(countriesAssociative).forEach(function (key) {
         var style = aStyle;
         if (window.location.href.includes("shipFromCountry=" + countriesAssociative[key])) {
             console.log("DEPO changed to " + key);
@@ -49,9 +49,13 @@
         innerContent += "|<a style=\"" + style + "\" href=\"javascript:(function(){ document.cookie = 'customDEPO=" + countriesAssociative[key] + "'; var urlParams = new URLSearchParams(window.location.search);urlParams.set('shipFromCountry', '" + countriesAssociative[key] + "');window.location.search = urlParams; })();\"> " + key + " </a>|";
     });
 
+    //combined delivery
+    var CDYES = "<a href=\"javascript:(function(){ document.cookie = 'CombinedDeliverySelect=n';var urlParams2 = new URLSearchParams(window.location.search);urlParams2.set('isCombinedDelivery', 'n');window.location.search = urlParams2; })()\">YES</a>";
+    var CDNO = "<a href=\"javascript:(function(){ document.cookie = 'CombinedDeliverySelect=y';var urlParams2 = new URLSearchParams(window.location.search);urlParams2.set('isCombinedDelivery', 'y');window.location.search = urlParams2; })()\">NO</a>";
+    var CDSelected = (new URLSearchParams(window.location.search).get("isCombinedDelivery") == "y") ? CDYES : CDNO;
+    innerContent += "<br>Combined Delivery " + CDSelected;
     countries.innerHTML = "<span style='" + divStyle + "'>Ship from " + innerContent + "</span>";
     document.body.appendChild(countries);
-
     ///////////////////////////////////////////////////////////////////////
     /* on url change -> 2nd, 3rd..... Xth page */
     if (window.onurlchange === null) { // feature is supported
@@ -59,6 +63,7 @@
             console.log(info.url);
             var urlParams = new URLSearchParams(window.location.search);
             urlParams.set('shipFromCountry', getCookie("customDEPO"));
+            urlParams.set('isCombinedDelivery', getCookie("CombinedDeliverySelect"));
             window.location.search = urlParams;
             console.log("DEPO change to " + getCookie("customDEPO"));
         });
